1fcdcb54b8f99f1b238477cf41259b7ec1523668
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-02-07 13:07:59 -0300

    Animações parte 02

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,10 @@ function start() {
   
     jogo.pressionou = [];
 
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 350);
+  var podeAtirar = true;
+
   //Verifica se o usuário pressionou alguma tecla	
 	$(document).keydown(function(e){
     jogo.pressionou[e.which] = true;
@@ -35,6 +39,9 @@ function start() {
   function loop() {
     movefundo();
     movejogador();
+    moveinimigo1();
+    moveinimigo2();
+    moveamigo();
   } // Fim da função loop()
   
   
@@ -89,12 +96,88 @@ function start() {
     }
     
     
-    
     if (jogo.pressionou[TECLA.D]) {
       
       //Chama função Disparo	
+      disparo();
     }
   
   } // fim da função movejogador()
+
+
+  // função que move o inimigo 1
+  function moveinimigo1() {
+
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left",posicaoX-velocidade);
+    $("#inimigo1").css("top",posicaoY);
+      
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 350);
+      $("#inimigo1").css("left",720);
+      $("#inimigo1").css("top",posicaoY);
+        
+    }
+  } //Fim da função moveinimigo1()
+
+
+  // função que move o inimigo 2
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left",posicaoX - 3);
+    
+    if (posicaoX <= 0) {
+      $("#inimigo2").css("left",775);
+    }
+  } // Fim da função moveinimigo2()
+
+
+  // função que move o inimigo 3
+  function moveamigo() {
+	
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left",posicaoX + 1);
+          
+    if (posicaoX > 906) {
+      $("#amigo").css("left",0);    
+    }
+  
+  } // fim da função moveamigo()
+
+
+  // função do disparo
+  function disparo() {
+	
+    if (podeAtirar === true) {
+      
+      podeAtirar = false;
+    
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX= parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro = topo + 37;
+      $('#fundoGame').append("<div id='disparo'></div");
+      $('#disparo').css('top',topoTiro);
+      $('#disparo').css('left',tiroX);
+      
+      var tempoDisparo = window.setInterval(executaDisparo, 30);
+    
+    } //Fecha podeAtirar
+  
+   
+    function executaDisparo() {
+      posicaoX = parseInt($('#disparo').css('left'));
+      $('#disparo').css('left',posicaoX + 15); 
+    
+      if (posicaoX > 900) {    
+        window.clearInterval(tempoDisparo);
+        tempoDisparo = null;
+        $('#disparo').remove();
+        podeAtirar = true;        
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
+    
+
 
 } // fim da função start()
